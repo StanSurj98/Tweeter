d367e2fed533da4f02bb8d9a90a7262456ddf492
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-13 17:00:29 -0600

    refactor the CSS, made sense of parent/child rules for clarity

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,7 @@
+// Make sure the HTML fully loads first before we run the script
+$(document).ready(() => {
+  console.log(`Document ready && JQuery working`);
+
+  // Next we want an event on the form for tweets to take place
+  const textBox = $(document.)
+})
