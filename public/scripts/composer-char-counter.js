d3b230625d65942836e56c492e6041bc909ebdef
Stanley Surjanto <stan.surjanto@gmail.com>
2022-09-13 20:56:39 -0600

    add new tweet containerwith article tag. add new tweet-container.css in styles

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,27 @@
 // Make sure the HTML fully loads first before we run the script
-$(document).ready(() => {
+$(document).ready(function() {
   console.log(`Document ready && JQuery working`);
 
   // Next we want an event on the form for tweets to take place
-  const textBox = $(document.)
-})
+  // $( ".new-tweet" ).on("keyup", "textarea", function() {
+  //   console.log($(this).val().length); // gets value of THIS text-area, and its length of chars
+  // });
+
+  $("#tweet-text-area").keyup(function() {
+    let textLength = $(this).val().length;
+    let counter = $(this).next().find("output");
+
+    // Now we add logic for turning the text red by adding a class if textLength is > 140
+    if (textLength > 140) {
+      // new css class that will turn the color: red
+      counter.addClass("text-red");
+      // make sure we still show the counter update, will be negative if > 140
+      counter.text(140 - textLength);
+    } else {
+      counter.removeClass("text-red");
+      counter.text(140 - textLength);
+    }
+    
+  });
+
+});
